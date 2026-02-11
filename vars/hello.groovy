16ec4d749e0ae8697e75740b60e7abd50ba37e01
Mohit Singh <68845963+Mohit50cyber@users.noreply.github.com>
2026-02-11 11:34:33 +0530

    w

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,5 @@
 def call(){
 echo "Hello World !!!!!!!! , today I created this shared library which will help me get output in Jenkins ...."
+  echo "So sorry , I committed this but it's good to be committed in jenkins"
+  echo "Now I can commit anything straight away , no need to do manual setups "
 }
